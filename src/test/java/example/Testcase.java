ed7df0c60babdb8e6b837079ef185f3d0639da2c
xuning <xuning@didichuxing.com>
2017-06-13 16:50:36 +0800

    hikari usage

---
+++ src/test/java/example/Testcase.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2017 example authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"): you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http: *www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+// Created by xuning on 2017/6/13
+
+package example;
+
+import org.junit.Test;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import java.sql.Connection;
+
+public class Testcase {
+  @Test
+  void testConnectionPool() throws Exception {
+    HikariConfig config = new HikariConfig();
+    config.setJdbcUrl("jdbc:mysql://localhost:3306/simpsons");
+    config.setUsername("bart");
+    config.setPassword("51mp50n");
+    config.addDataSourceProperty("cachePrepStmts", "true");
+    config.addDataSourceProperty("prepStmtCacheSize", "250");
+    config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
+
+    HikariDataSource ds = new HikariDataSource(config);
+    try (Connection connection = ds.getConnection()) {
+      // use the connection
+    }
+
+  }
+}
